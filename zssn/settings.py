3eb2d1e60634c5f21e34bc7913616f3b179b1525
Marcos Raulino <marcosfsraulino@gmail.com>
2023-09-04 00:54:46 -0300

    fix: fix ALLOWED_HOSTS allowing zssn.onrender.com

--- zssn/settings.py
+++ zssn/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://zssn.onrender.com']
+ALLOWED_HOSTS = ['zssn.onrender.com']
 
 
 # Application definition
