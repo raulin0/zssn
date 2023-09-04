5f04ea175c9da3625a182a864429a7ce9c681c29
Marcos Raulino <marcosfsraulino@gmail.com>
2023-09-04 01:49:23 -0300

    build: removes whitenoise from the project dependencies and set staticfiles_urlpatterns to serve static files

--- zssn/urls.py
+++ zssn/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from api.views import SurvivorViewSet
 
@@ -28,3 +29,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
